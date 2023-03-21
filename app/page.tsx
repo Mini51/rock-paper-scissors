dafdfc8e0a9b4e5601fbf45d9b7afb0ed7607d6c
Mini <80361877+Mini51@users.noreply.github.com>
2023-03-20 21:52:41 -0400

    Added new styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,28 @@ import { HandFist, Scroll, Scissors } from "@phosphor-icons/react";
 import BlankComponent from "@rock-paper-scissors/components/BlankComponent";
 
 export default function Home() {
+
   return (
-    <main className="flex">
-      {/* Rock */}
-      <BlankComponent
-        name={"Rock"}
-        icon={<HandFist size={75} weight="fill" color="white" />}
-        color="edae49"
-      />
-
-      {/* Rock */}
-      <BlankComponent
-        name={"Paper"}
-        icon={<Scroll size={75} weight="fill" color="white" />}
-        color="d1495b"
-      />
-
-      {/* Rock */}
-      <BlankComponent
-        name={"Scissors"}
-        icon={<Scissors size={75} weight="fill" color="white" />}
-        color="d1495b"
-      />
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="flex flex-row justify-center bg-slate-800 rounded-xl drop-shadow-2  xl">
+          <BlankComponent
+            name={"Rock"}
+            icon={<HandFist size={75} weight="fill" color="white" />}
+            color="edae49"
+          />
+          <BlankComponent
+            name={"Paper"}
+            icon={<Scroll size={75} weight="fill" color="white" />}
+            color="edae49"
+          />
+          <BlankComponent
+            name={"Scissors"}
+            icon={<Scissors size={75} weight="fill" color="white" />}
+            color="edae49"
+          />
+        </div>
+      </main>
+    </div>
   );
 }
